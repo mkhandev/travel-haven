510b9632c00c543d7741398bc257293dbf4326cf
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-08-17 12:50:18 +0600

    add user navigation
    Co-authored-by: Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import UserIcon from "./UserIcon";
+import { menuLinks } from "@/utils/Links";
 
-const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
-};
-
+function LinksDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        {menuLinks.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="w-full capitalize">
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 export default LinksDropdown;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const UserIcon = () => {
-  return <div>UserIcon</div>;
-};
-
+import { LuUser } from "react-icons/lu";
+async function UserIcon() {
+  return <LuUser className="w-6 h-6 text-white rounded-full bg-primary" />;
+}
 export default UserIcon;
