3c6b3e84932f8d1af050ed3d8a53a5ad0b445252
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-08-25 01:42:42 +0600

    add nav serach

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,46 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+
+    replace(`/?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams]);
+
   return (
     <Input
       type="text"
       placeholder="find a property..."
       className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 };
