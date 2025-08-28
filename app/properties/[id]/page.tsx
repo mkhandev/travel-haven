6b8634ad7c34000bc5e4b7a29b50928039dc0274
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-08-28 13:46:50 +0600

    add booking calender

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
 import ShareButton from "@/components/properties/ShareButton";
@@ -30,6 +32,20 @@ const PropertyDetailsPage = async ({
         </div>
       </header>
       <ImageContainer mainImage={property.image} name={property.name} />
+
+      <section className="mt-12 lg:grid lg:grid-cols-12 gap-x-12">
+        <div className="lg:col-span-8">
+          <div className="flex items-center gap-x-4">
+            <h1 className="text-xl font-bold">{property.name} </h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center lg:col-span-4">
+          {/* calendar */}
+          <BookingCalendar />
+        </div>
+      </section>
     </section>
   );
 };
